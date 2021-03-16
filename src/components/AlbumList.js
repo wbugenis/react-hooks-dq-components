25373f8854c6ab759c4f2036f4f4c5f1877e3ead
William Bugenis <wbugenis@gmail.com>
2021-03-16 10:08:04 -0400

    done

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,8 +4,18 @@ import { albums } from "../data/albums";
 
 function AlbumList() {
   return (
-    <section className="albums">
-      <AlbumCard
+    <section className="albums">{
+      albums.map(album => {
+
+        return <AlbumCard 
+          key = {album.id}
+          name = {album.name}
+          image = {album.image}
+          genre = {album.genre}
+          />
+      })
+    }
+      {/* <AlbumCard
         name="Classic Tuna Cans Being Opened"
         image="https://target.scene7.com/is/image/Target/GUEST_360eeed2-1747-4b15-8394-a22c7cd12230?wid=488&hei=488&fmt=pjpeg"
         genre="Cats"
@@ -24,7 +34,7 @@ function AlbumList() {
         name="Aloha K.K."
         image="https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742"
         genre="Dogs"
-      />
+      /> */}
     </section>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,12 @@
 import React from "react";
+import GenrePicker from "./GenrePicker"
+import AlbumList from "./AlbumList"
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return <main>
+    <GenrePicker />
+    <AlbumList />
+  </main>;
 }
 
 export default MainContent;
